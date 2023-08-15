0e0d07e8ad2f5a978bf781937b7f70099f3a26bb
Diego Mendoza <diegocoronado_@outlook.com>
2023-08-15 00:24:27 -0700

    Added progress circle

--- tabago/tabago/ContentView.swift
+++ tabago/tabago/ContentView.swift
@@ -7,31 +7,40 @@
 
 import SwiftUI
 import AVFoundation
+import MediaPlayer
 
 struct ContentView: View {
     var primaryColor = Color(red: 62/255, green: 207/255, blue: 142/255)
     var secondaryColor = Color(red: 24/255, green: 24/255, blue: 24/255)
     
     @State var activeTimeMinutes: Int = 0
-    @State var activeTimeSeconds: Int = 10
+    @State var activeTimeSeconds: Int = 5
     @State var passiveTimeMinutes: Int = 0
-    @State var passiveTimeSeconds: Int = 12
+    @State var passiveTimeSeconds: Int = 5
     @State var numberOfRounds: Int = 10
-    @State var status: String = "passive"
+    @State var status: String = "initial"
     @State var showConfiguration = false 
     @State var currentRound = 1
     @State var totalRounds = 10
     
     @State var activeTimeSound = "Alarm"
     @State var passiveTimeSound = "Alarm"
-    @State var seconds = 12;
+    @State var seconds = 5;
     @State var minutes = 0;
     @State var timeRunning = false;
+    
+    @State var circleParts = 0.20
+    @State var circleDivision = 20.0
+    @State var circleProgress = 1
+    
+    @State var timerStarted = false
     @State private var timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect();
     
     
     var backgroundColor: Color {
-//        36,180,126
+        if status == "initial" {
+            return Color.blue
+        }
         return status == "passive" ? secondaryColor : Color(red: 36/255, green: 180/255, blue: 126/255)
     }
     
@@ -43,32 +52,10 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             VStack {
-                HStack {
-                    Button {
-                        restart()
-                    } label: {
-                        Image(systemName: "gobackward")
-                            .font(.system(size: 30))
-                            .foregroundColor(labelColors)
-                    }
-                    .disabled(status == "active")
-                    .padding(EdgeInsets(top: 0, leading: 0, bottom: -30, trailing: 0))
-                    Spacer()
-                        
-                    Button {
-                        showConfiguration = true;
-                    } label: {
-                        Image(systemName: "gear.badge")
-                            .font(.system(size: 30))
-                            .foregroundColor(labelColors)
-                    }
-                    .disabled(status == "active")
-                    .padding(EdgeInsets(top: 0, leading: 0, bottom: -30, trailing: 0))
-                }
-                .padding()
+                actionButtons
                 Spacer()
                 Text("\(formatTimer())")
-                    .font(.system(size: 70))
+                    .font(.system(size: 120))
                     .onReceive(timer) { _ in
                         if timeRunning && currentRound < totalRounds + 1 {
                             seconds -= 1;
@@ -78,6 +65,7 @@ struct ContentView: View {
                                 seconds = 59
                                 minutes -= 1;
                             }
+                            updateCircle()
                         }
                     }
                     .foregroundColor(labelColors)
@@ -100,9 +88,8 @@ struct ContentView: View {
             })
             .background(backgroundColor)
             .onAppear {
-                numberOfRounds = 10
-                activeTimeSeconds = 10
                 UIApplication.shared.isIdleTimerDisabled = true
+                MPVolumeView.setVolume(7.00)
             }
         }
     }
@@ -128,18 +115,27 @@ struct ContentView: View {
     }
     
     func toggleTimer() {
+        var total: Double = 0.0
         if (status == "passive") {
             seconds = activeTimeSeconds
             minutes = activeTimeMinutes
             status = "active"
-            if currentRound < totalRounds + 1 {
-                nextRound()
-            }
+            total = Double((activeTimeMinutes * 60) + activeTimeSeconds)
         } else {
             seconds = passiveTimeSeconds
             minutes = passiveTimeMinutes
             status = "passive"
+            total = Double((passiveTimeMinutes * 60) + passiveTimeSeconds)
+            if currentRound < totalRounds + 1 && timerStarted {
+                nextRound()
+            }
+            if currentRound == 1 {
+                timerStarted = true
+            }
         }
+        circleParts = 0.0
+        circleDivision = Double(100 / total)
+        circleProgress = 1
         playMySound()
 
     }
@@ -154,10 +150,19 @@ struct ContentView: View {
     }
     
     func restart() {
-        seconds = passiveTimeSeconds
-        minutes = passiveTimeMinutes
+        seconds = 5
+        minutes = 0
         currentRound = 1
+        status = "initial"
+        circleParts = 0.0
     }
+    
+    func updateCircle() {
+        circleParts = circleDivision * Double(circleProgress) / 100
+        print(circleParts, circleDivision, circleProgress)
+        circleProgress += 1
+    }
+    
     func playMySound() {
         // 1323 - train
         // 1325 - flauta
@@ -204,8 +209,17 @@ struct ContentView: View {
                     .font(.system(size: 40))
                     .frame(width: 200, height: 200)
                     .overlay(content: {
-                        RoundedRectangle(cornerRadius: 100)
-                                    .stroke(labelColors, lineWidth: 2)
+                        ZStack {
+                            Circle()
+                                .stroke(labelColors, lineWidth: 10)
+                                .opacity(0.5)
+                            Circle()
+                                .trim(from: 0, to: circleParts)
+                                .stroke(labelColors, lineWidth: 10)
+                                .rotationEffect(.degrees(-90))
+                                .animation(.easeIn, value: circleParts)
+                        }
+                        
                     })
                     .foregroundColor(labelColors)
                     .padding()
@@ -226,10 +240,49 @@ struct ContentView: View {
         
     }
     
+    var actionButtons: some View {
+        HStack {
+            Button {
+                restart()
+            } label: {
+                Image(systemName: "gobackward")
+                    .font(.system(size: 30))
+                    .foregroundColor(labelColors)
+            }
+            .disabled(timeRunning)
+            .padding(EdgeInsets(top: 0, leading: 0, bottom: -30, trailing: 0))
+            Spacer()
+                
+            Button {
+                showConfiguration = true;
+            } label: {
+                Image(systemName: "gear.badge")
+                    .font(.system(size: 30))
+                    .foregroundColor(labelColors)
+            }
+            .disabled(timeRunning)
+            .padding(EdgeInsets(top: 0, leading: 0, bottom: -30, trailing: 0))
+        }
+        .padding()
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+
+//Update system volume
+extension MPVolumeView {
+    static func setVolume(_ volume: Float) {
+        let volumeView = MPVolumeView()
+        let slider = volumeView.subviews.first(where: { $0 is UISlider }) as? UISlider
+
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.01) {
+            slider?.value = volume
+        }
     }
 }
